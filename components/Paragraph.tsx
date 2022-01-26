b79e78806eb14e04201ef136a21749fc4e273e1c
dominon12 <youneedmax@gmail.com>
2022-01-26 16:40:55 +0100

    add product page

--- components/Paragraph.tsx
+++ components/Paragraph.tsx
@@ -4,10 +4,15 @@ import styled from "styled-components";
 const Text = styled.p`
   margin: 0;
   font-size: 1rem;
+  line-height: 1.6;
 `;
 
-const Paragraph: FC = ({ children }) => {
-  return <Text>{children}</Text>;
+interface Props {
+  className?: string;
+}
+
+const Paragraph: FC<Props> = ({ children, className }) => {
+  return <Text className={className}>{children}</Text>;
 };
 
 export default Paragraph;
