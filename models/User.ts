c9db5065887be9b9bc90af72daad4701e426dafc
dominon12 <youneedmax@gmail.com>
2022-02-09 18:27:39 +0100

    add User model

---
+++ models/User.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAdmin: Boolean,
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
