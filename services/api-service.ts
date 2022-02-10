c9c8cff3a1ab39924a7d30e414db4299c2f72a70
dominon12 <youneedmax@gmail.com>
2022-02-10 14:28:04 +0100

    change api base url

--- services/api-service.ts
+++ services/api-service.ts
@@ -1,6 +1,7 @@
 // const apiBaseUrl = "http://127.0.0.1:8000/api/v1/";
 // const apiBaseUrl = "https://dominon12.pythonanywhere.com/api/v1/";
-const apiBaseUrl = "http://localhost:3000/api/v1/";
+// const apiBaseUrl = "http://localhost:3000/api/v1/";
+const apiBaseUrl = "https://store-helper.vercel.app/api/v1/";
 
 export const URLS = {
   products: apiBaseUrl + "products/",
